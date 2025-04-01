910b71f1da4edb1457e635e3ecf6c03de0e5cd5f
Leonambr <leonam43@hotmail.com>
2025-04-01 14:45:34 -0300

    gladsetup home

---
+++ build/CMakeFiles/OpenGL_API.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_API.dir/src/sandbox.cpp.obj"
+  "CMakeFiles/OpenGL_API.dir/src/sandbox.cpp.obj.d"
+  "CMakeFiles/OpenGL_API.dir/vendor/glad/src/glad.c.obj"
+  "CMakeFiles/OpenGL_API.dir/vendor/glad/src/glad.c.obj.d"
+  "OpenGL_API.exe"
+  "OpenGL_API.exe.manifest"
+  "OpenGL_API.pdb"
+  "libOpenGL_API.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_API.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,12 +1,23 @@
+#include <string>
 #include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void vertex();
+void vertexBuffer();
 
 const GLuint width = 800;
 const GLuint height = 600;
+GLuint VBO, VAO, EBO;
+
+std::string vertex = R"(#version 330 core
+                        layout (location = 0) in vec3 aPos;
+
+                        void main()
+                        {
+                          gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+                          })";
 
 int main (int argc, char** argv)
 {
@@ -60,7 +71,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 } 
 
-void vertex()
+void vertexBuffer()
 {
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
@@ -68,8 +79,8 @@ void vertex()
          0.0f,  0.5f, 0.0f
     }; 
     
-    GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
 }
