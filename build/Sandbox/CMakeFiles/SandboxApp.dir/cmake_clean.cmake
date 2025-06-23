a23b0b1182ac357194a6ff1649c2fa253dd494fa
LeonamBr <leonam43@otmail.com>
2025-06-23 03:22:08 -0300

    Git ignore update

---
+++ build/Sandbox/CMakeFiles/SandboxApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Leonam/OpenGLAPI/bin/SandboxApp.exe"
+  "C:/Leonam/OpenGLAPI/bin/SandboxApp.exe.manifest"
+  "C:/Leonam/OpenGLAPI/bin/SandboxApp.pdb"
+  "CMakeFiles/SandboxApp.dir/src/sandboxApp.cpp.obj"
+  "CMakeFiles/SandboxApp.dir/src/sandboxApp.cpp.obj.d"
+  "libSandboxApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SandboxApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
